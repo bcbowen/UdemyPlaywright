ea5170256e0504485b8aaaeb38011aedbf2c4ddc
Ben Bowen <bcbowen@gmail.com>
2023-09-23 13:21:35 -0400

    Got the sameple api e2e code working

--- tests/WebApiPart1.spec.js
+++ tests/WebApiPart1.spec.js
@@ -5,15 +5,14 @@ const loginPayload = {
   userPassword: "Iamking@000",
 };
 const orderPayload = {
-  orders: [{ country: "India", productOrderId: "6262e95ae26b7e1a10e89bf0" }],
+  orders: [{ country: "India", productOrderedId: "64c1388c7244490f958d0955" }],
 };
-let token;
-let orderId;
+let response;
 let apiContext;
 test.beforeAll(async () => {
   apiContext = await request.newContext();
   const apiUtils = new ApiUtils(apiContext, loginPayload);
-  await apiUtils.createOrder(orderPayload);
+  response = await apiUtils.createOrder(orderPayload);
 });
 
 //test.beforeEach(async () => {});
@@ -22,81 +21,29 @@ test("Client App Login", async ({ page }) => {
   const Utils = new ApiUtils(apiContext);
   page.addInitScript((value) => {
     window.localStorage.setItem("token", value);
-  }, token);
+  }, response.token);
   const productName = "zara coat 3";
   const userid = "anshika@gmail.com";
   const password = "Iamking@000";
   const products = page.locator(".card-body");
   await page.goto("https://rahulshettyacademy.com/client");
 
-  /*
-  
-  await page.locator("#userEmail").type(userid);
-  await page.locator("#userPassword").type(password);
-  await page.locator("#login").click();
-  await page.waitForLoadState("networkidle");
-  */
-  /*
-  await page.locator(".card-body b").first().waitFor();
-  const count = await products.count();
-  console.log(count);
-  console.log(productName);
-  //await page.pause();
-  for (let i = 0; i < count; i++) {
-    if ((await products.nth(i).locator("b").textContent()) === productName) {
-      // add to cart
-      await products.nth(i).locator("text= Add To Cart").click();
-      console.log("found");
-      break;
-    }
-  }
-  await page.locator("[routerlink*='cart']").click();
-  await page.locator("div li").first().waitFor();
-  expect(
-    page.locator(`h3:has-text("${productName}")`).isVisible()
-  ).toBeTruthy();
-  await page.locator("text=Checkout").click();
-  await page.locator("[placeholder*='Country']").type("ind", { delay: 100 });
-  const options = page.locator(".ta-results");
-  await options.waitFor();
-  const optionsCount = await options.locator("button").count();
-  for (let i = 0; i < optionsCount; i++) {
-    let text = await options.locator("button").nth(i).textContent();
-    if (text === " India") {
-      await options.locator("button").nth(i).click();
-      break;
-    }
-  }
-
-  await expect(page.locator(".user__name [type='text']").first()).toHaveText(
-    userid
-  );
-  await page.locator(".action__submit").click();
-
-  await expect(page.locator(".hero-primary")).toHaveText(
-    " Thankyou for the order. "
-  );
-  const orderNumber = await page
-    .locator(".em-spacer-1 .ng-star-inserted")
-    .textContent();
-  console.log(orderNumber);
-*/
   // exercise: find order from order history and go to view order
   await page.locator("button[routerlink*='myorders']").click();
-  // body > app-root > app-myorders > div.container.table-responsive.py-5 > table
-  // body > app-root > app-myorders > div.container.table-responsive.py-5 > table > thead
-  const ordersTable = await page.locator("table.ng-star-inserted tbody");
-  await page.pause();
-  const orderCount = await ordersTable.locator("tr").count();
-  console.log(`Order count: ${orderCount}`);
 
-  for (let i = 0; i < orderCount; i++) {
-    const orderRow = await ordersTable.locator("tr").nth(i);
-
-    if (await orderRow.locator("th").textContent().includes(orderId)) {
-      console.log("Found");
-      await orderRow.locator(".btn-primary").click();
+  // without the following line 0 rows will be found:
+  await page.locator("tbody").waitFor();
+  const orderRows = await page.locator("table.ng-star-inserted tbody tr");
+  const rowCount = await orderRows.count();
+  console.log(`html row count: ${rowCount}`);
+  for (let i = 0; i < rowCount; i++) {
+    let row = orderRows.nth(i);
+    let rowOrderId = await row.locator("th").textContent();
+    console.log(`html: ${rowOrderId} json: ${response.orderId}`);
+    if (rowOrderId.includes(response.orderId)) {
+      await row.locator(".btn-primary").click();
       break;
     }
   }
+  await page.pause();
 });
--- tests/utils/ApiUtils.js
+++ tests/utils/ApiUtils.js
@@ -10,25 +10,28 @@ class ApiUtils {
       { data: this.loginPayload }
     );
     const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
+    const token = loginResponseJson.token;
     console.log(token);
     return token;
   }
 
   async createOrder(orderPayload) {
+    let response = {};
+    response.token = await this.getToken();
     const orderResponse = await this.apiContext.post(
       "https://rahulshettyacademy.com/api/ecom/order/create-order",
       {
         data: orderPayload,
         headers: {
-          Authorization: tihs.getToken(),
+          Authorization: response.token,
           "Content-Type": "application/json",
         },
       }
     );
 
     const orderResponseJson = await orderResponse.json();
-    return orderResponseJson.orders[0];
+    response.orderId = orderResponseJson.orders[0];
+    return response;
   }
 }
 
