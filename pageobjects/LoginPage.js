4a3de9167ef843f5884c5b50377db482426bf110
Ben Bowen <bcbowen@gmail.com>
2023-09-28 21:54:18 -0400

    Troubleshooting sample PO pages

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -1,9 +1,11 @@
 class LoginPage {
+
   constructor(page) {
     this.page = page;
-    this.signInButton = page.locator("[value='Login']");
+    this.signInbutton = page.locator("[value='Login']");
     this.userName = page.locator("#userEmail");
     this.password = page.locator("#userPassword");
+
   }
 
   async goTo() {
@@ -13,10 +15,10 @@ class LoginPage {
   async validLogin(username, password) {
     await this.userName.type(username);
     await this.password.type(password);
-    await this.page.pause();
-    await this.signInButton.click();
-    await this.page.waitForLoadState("networkidle");
-  }
-}
+    await this.signInbutton.click();
+    await this.page.waitForLoadState('networkidle');
 
+  }
+
+}
 module.exports = { LoginPage };
--- tests/ClientAppPageObjectImpl.spec.js
+++ tests/ClientAppPageObjectImpl.spec.js
@@ -1,66 +1,30 @@
 const { test, expect } = require("@playwright/test");
-const { LoginPage } = require("../pageobjects/LoginPage");
-const { DashBoardPage } = require("../pageobjects/DashBoardPage");
+const { PageObjectManager } = require("../pageobjects/PageObjectManager");
 
 test("Client App Login", async ({ page }) => {
-  const productName = "zara coat 3";
+  const pageObjectManager = new PageObjectManager(page);
+  //js file- Login js, DashboardPage
   const username = "anshika@gmail.com";
-  const password = "Iamking@000";
+  const password = "Iamking@000"
+  const productName = 'Zara Coat 4';
   const products = page.locator(".card-body");
-  const loginPage = new LoginPage(page);
+  const loginPage = pageObjectManager.getLoginPage();
   await loginPage.goTo();
   await loginPage.validLogin(username, password);
-
-  /*
-  const dashboardPage = new DashBoardPage(page);
+  const dashboardPage = pageObjectManager.getDashboardPage();
   await dashboardPage.searchProductAddCart(productName);
   await dashboardPage.navigateToCart();
- 
-  await page.locator("div li").first().waitFor();
-  expect(
-    page.locator(`h3:has-text("${productName}")`).isVisible()
-  ).toBeTruthy();
-  await page.locator("text=Checkout").click();
-  await page.locator("[placeholder*='Country']").type("ind", { delay: 100 });
-  const options = page.locator(".ta-results");
-  await options.waitFor();
-  const optionsCount = await options.locator("button").count();
-  for (let i = 0; i < optionsCount; i++) {
-    let text = await options.locator("button").nth(i).textContent();
-    if (text === " India") {
-      await options.locator("button").nth(i).click();
-      break;
-    }
-  }
 
-  await expect(page.locator(".user__name [type='text']").first()).toHaveText(
-    username
-  );
-  await page.locator(".action__submit").click();
+  const cartPage = pageObjectManager.getCartPage();
+  await cartPage.VerifyProductIsDisplayed(productName);
+  await cartPage.Checkout();
 
-  await expect(page.locator(".hero-primary")).toHaveText(
-    " Thankyou for the order. "
-  );
-  const orderNumber = await page
-    .locator(".em-spacer-1 .ng-star-inserted")
-    .textContent();
-  console.log(orderNumber);
-
-  // exercise: find order from order history and go to view order
-  await page.locator("button[routerlink*='myorders']").click();
-  // body > app-root > app-myorders > div.container.table-responsive.py-5 > table
-  // body > app-root > app-myorders > div.container.table-responsive.py-5 > table > thead
-  await page.locator("tbody").waitFor();
-  const rows = await page.locator("tbody tr");
-
-  for (let i = 0; i < (await rows.count()); ++i) {
-    const rowOrderId = await rows.nth(i).locator("th").textContent();
-    if (orderNumber.includes(rowOrderId)) {
-      await rows.nth(i).locator("button").first().click();
-      break;
-    }
-  }
-  const orderIdDetails = await page.locator(".col-text").textContent();
-  expect(orderNumber.includes(orderIdDetails)).toBeTruthy();
-  */
+  const ordersReviewPage = pageObjectManager.getOrdersReviewPage();
+  await ordersReviewPage.searchCountryAndSelect("ind", "India");
+  const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+  console.log(orderId);
+  await dashboardPage.navigateToOrders();
+  const ordersHistoryPage = pageObjectManager.getOrdersHistoryPage();
+  await ordersHistoryPage.searchOrderAndSelect(orderId);
+  expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
 });
