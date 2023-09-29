1b465e74ff19aa36422de00588b4bc832607e76f
Ben Bowen <bcbowen@gmail.com>
2023-09-28 22:10:29 -0400

    Add test data

--- tests/ClientAppPageObjectImpl.spec.js
+++ tests/ClientAppPageObjectImpl.spec.js
@@ -1,22 +1,20 @@
 const { test, expect } = require("@playwright/test");
 const { PageObjectManager } = require("../pageobjects/PageObjectManager");
+const dataSet = JSON.parse(JSON.stringify(require("../utils/placeOrderTestData.json")));
 
 test("Client App Login", async ({ page }) => {
   const pageObjectManager = new PageObjectManager(page);
-  //js file- Login js, DashboardPage
-  const username = "anshika@gmail.com";
-  const password = "Iamking@000"
-  const productName = 'Zara Coat 4';
+
   const products = page.locator(".card-body");
   const loginPage = pageObjectManager.getLoginPage();
   await loginPage.goTo();
-  await loginPage.validLogin(username, password);
+  await loginPage.validLogin(dataSet.username, dataSet.password);
   const dashboardPage = pageObjectManager.getDashboardPage();
-  await dashboardPage.searchProductAddCart(productName);
+  await dashboardPage.searchProductAddCart(dataSet.productName);
   await dashboardPage.navigateToCart();
 
   const cartPage = pageObjectManager.getCartPage();
-  await cartPage.VerifyProductIsDisplayed(productName);
+  await cartPage.VerifyProductIsDisplayed(dataSet.productName);
   await cartPage.Checkout();
 
   const ordersReviewPage = pageObjectManager.getOrdersReviewPage();
